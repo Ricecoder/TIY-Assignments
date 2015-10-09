63a35b214d4304c6a3b4945bb125f61c59a196ca
Lacey R <laceyrice@me.com>
2015-10-09 14:34:47 -0400

    adds and removes active from specific panel

--- halftime/dollar-menu/src/js/main.js
+++ halftime/dollar-menu/src/js/main.js
@@ -1,4 +1,18 @@
 $('a[href="#lovely"]').on('click', function() {
-  console.log('tracer');
-  $('ul.panels').addClass('active');
+  $('ul.panels#lovely').toggleClass('active');
+
+
+
 });
+
+
+$('a[href="#delicious"]').on('click', function() {
+  console.log("delicious!");
+  $('ul.panels#delicious').toggleClass('active');
+})
+
+
+$('a[href="#fine"]').on('click', function() {
+  console.log("fine!");
+  $('ul.panels#fine').toggleClass('active');
+})
